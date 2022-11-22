d20ebb8feaad2a10e6bfe1f0e5882a06f97c01e9
Omprasad143 <om16061998@gmail.com>
2022-11-22 15:34:02 +0000

    solution

---
+++ index.js
@@ -0,0 +1,23 @@
+const Box = (props) => {
+  //  Write your code here.
+  const { text, className } = props;
+  return (
+    <div className={`box ${className}`}>
+      <p className="box-title">{text}</p>
+    </div>
+  );
+};
+
+const element = (
+  //  Write your code here.
+  <div className="boxes-app-container">
+    <h1 className="heading">Boxes</h1>
+    <div className="boxes-container">
+      <Box text="Small" className="small-box" />
+      <Box text="Medium" className="medium-box" />
+      <Box text="Large" className="large-box" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
